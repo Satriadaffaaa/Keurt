d053fe75383b707ca32b3c5d66d31142d86fbaf0
salmannnnn12 <alfaridzisalman757@gmail.com>
2024-05-18 23:58:33 +0700

    nambah slide bar

--- resources/views/Component/sidebar.blade.php
+++ resources/views/Component/sidebar.blade.php
@@ -40,6 +40,14 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>
+                            Bagas
+                        </p>
+                    </a>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
